60a8314fdd6521513b95f2f542b47040b50e3159
Deni Rohimat <rohimatdeni@gmail.com>
2024-11-24 13:29:54 +0700

    implement grouped list

--- lib/presentation/screens/home/home_content.dart
+++ lib/presentation/screens/home/home_content.dart
@@ -18,35 +18,72 @@ class HomeContent extends GetView<HomeController> {
       } else if (controller.contacts.isEmpty) {
         return const Center(child: Text('No contacts found.'));
       } else {
+        final groupedContacts = _groupContactsByFirstChar(controller.contacts);
         return RefreshIndicator(
           onRefresh: () async {
             await controller.fetchContacts();
           },
           child: ListView.builder(
-            itemCount: controller.contacts.length,
+            itemCount: groupedContacts.length,
             itemBuilder: (context, index) {
-              final contact = controller.contacts[index];
-              return ListTile(
-                leading: InitialAvatar(
-                  firstName: contact.firstName,
-                  lastName: contact.lastName,
-                ),
-                title: Text('${contact.firstName} ${contact.lastName}'),
-                onTap: () async {
-                  final result = await Get.to(
-                    () => const ContactDetailScreen(),
-                    arguments: contact,
-                    binding: ContactDetailBinding(),
-                  );
-                  if (result != null && result is Contact) {
-                    controller.contacts[index] = result;
-                  }
-                },
-              );
+              final group = groupedContacts.entries.elementAt(index);
+              return _buildGroup(context, group.key, group.value);
             },
           ),
         );
       }
     });
+  }
+
+  Map<String, List<Contact>> _groupContactsByFirstChar(List<Contact> contacts) {
+    final Map<String, List<Contact>> groupedContacts = {};
+    for (var contact in contacts) {
+      final firstChar = contact.firstName[0].toUpperCase();
+      if (!groupedContacts.containsKey(firstChar)) {
+        groupedContacts[firstChar] = [];
+      }
+      groupedContacts[firstChar]!.add(contact);
+    }
+    return groupedContacts;
+  }
+
+  Widget _buildGroup(BuildContext context, String groupKey, List<Contact> contacts) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  groupKey,
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                const Divider(),
+              ],
+            )),
+        ...contacts.map(
+          (contact) => ListTile(
+            leading: InitialAvatar(
+              firstName: contact.firstName,
+              lastName: contact.lastName,
+            ),
+            title: Text('${contact.firstName} ${contact.lastName}'),
+            onTap: () async {
+              final result = await Get.to(
+                () => const ContactDetailScreen(),
+                arguments: contact,
+                binding: ContactDetailBinding(),
+              );
+              if (result != null && result is Contact) {
+                final index = controller.contacts.indexOf(contact);
+                controller.contacts[index] = result;
+              }
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
