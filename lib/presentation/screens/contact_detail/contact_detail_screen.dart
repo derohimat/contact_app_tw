2b453f2c5c18b5022bf884a9fdc4c721a058f929
Deni Rohimat <rohimatdeni@gmail.com>
2024-11-24 14:33:38 +0700

    update input style

--- lib/presentation/screens/contact_detail/contact_detail_screen.dart
+++ lib/presentation/screens/contact_detail/contact_detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:contact_app_tw/styles/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,36 +12,96 @@ class ContactDetailScreen extends GetView<ContactDetailController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Contact Details'),
-      ),
+      appBar: AppBar(title: const Text('Contact Details')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            InitialAvatar(
-              firstName: controller.contact.firstName,
-              lastName: controller.contact.lastName,
-              size: 100.0,
-              textSize: 40.0,
+            Center(
+              child: InitialAvatar(
+                firstName: controller.contact.firstName,
+                lastName: controller.contact.lastName,
+                size: 100,
+                textSize: 40,
+              ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 24.0),
+            Text(
+              "Main Information",
+              style: TextStyle(color: AppColors.blue, fontStyle: FontStyle.italic, fontSize: 15.0),
+            ),
+            Divider(),
+            const SizedBox(height: 8.0),
+            Text(
+              "First Name",
+              style: TextStyle(color: AppColors.black, fontSize: 15.0),
+            ),
+            const SizedBox(height: 8.0),
             TextField(
               controller: controller.firstNameController,
-              decoration: const InputDecoration(labelText: 'First Name'),
+              decoration: const InputDecoration(
+                labelText: 'First Name',
+                prefixIcon: Icon(
+                  Icons.person_outline,
+                  color: AppColors.blue,
+                ),
+              ),
             ),
+            const SizedBox(height: 16.0),
+            Text(
+              "Last Name",
+              style: TextStyle(color: AppColors.black, fontSize: 15.0),
+            ),
+            const SizedBox(height: 8.0),
             TextField(
               controller: controller.lastNameController,
-              decoration: const InputDecoration(labelText: 'Last Name'),
+              decoration: const InputDecoration(
+                labelText: 'Last Name',
+                prefixIcon: Icon(
+                  Icons.person_outline,
+                  color: AppColors.blue,
+                ),
+              ),
             ),
+            const SizedBox(height: 24.0),
+            Text(
+              "Sub Information",
+              style: TextStyle(color: AppColors.blue, fontStyle: FontStyle.italic, fontSize: 15.0),
+            ),
+            Divider(),
+            const SizedBox(height: 8.0),
+            Text(
+              "Email",
+              style: TextStyle(color: AppColors.black, fontSize: 15.0),
+            ),
+            const SizedBox(height: 8.0),
             TextField(
               controller: controller.emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                prefixIcon: Icon(
+                  Icons.email_outlined,
+                  color: AppColors.blue,
+                ),
+              ),
               keyboardType: TextInputType.emailAddress,
             ),
+            const SizedBox(height: 16.0),
+            Text(
+              "Date of Birth",
+              style: TextStyle(color: AppColors.black, fontSize: 15.0),
+            ),
+            const SizedBox(height: 8.0),
             TextField(
               controller: controller.dobController,
-              decoration: const InputDecoration(labelText: 'Date of Birth'),
+              decoration: const InputDecoration(
+                labelText: 'Date of Birth',
+                prefixIcon: Icon(
+                  Icons.calendar_month_outlined,
+                  color: AppColors.blue,
+                ),
+              ),
               onTap: () async {
                 // Show date picker
                 final DateTime? pickedDate = await showDatePicker(
@@ -54,7 +115,7 @@ class ContactDetailScreen extends GetView<ContactDetailController> {
                 }
               },
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () async {
                 // Update contact
